39f70d66a32b293358b2fc8f2b10dfc3c3a71fbe
Selim Enes Erdogan <sellim81@gmail.com>
2022-05-10 18:15:43 +0300

    main sources

---
+++ src/Components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+import "./List.css";
+
+function List() {
+  return (
+    <div className="list">
+      <Form />
+    </div>
+  );
+}
+
+export default List;
